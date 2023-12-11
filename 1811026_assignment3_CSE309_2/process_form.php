3d97387f9fd02ece5a4927152b3042f6f71a6735
Noorjahan Sayeed <115143400+noorjahansayeed@users.noreply.github.com>
2023-12-11 20:39:20 +0600

    process_form.php

--- 1811026_assignment3_CSE309_2/process_form.php
+++ 1811026_assignment3_CSE309_2/process_form.php
@@ -1,5 +1,5 @@
 <?php
-// Connection to the database
+
 $servername = "your_database_server";
 $username = "your_database_username";
 $password = "your_database_password";
@@ -7,18 +7,16 @@ $dbname = "your_database_name";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Process form data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // Insert data into the database
+
     $sql = "INSERT INTO contact_form (name, email, message) VALUES ('$name', '$email', '$message')";
 
     if ($conn->query($sql) === TRUE) {
